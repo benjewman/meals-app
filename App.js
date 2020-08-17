3e01e229dc0cce53fa21c73cc71a60b92bace03d
benjewman <bennyadave@gmail.com>
2020-08-17 18:53:10 -0400

    Add empty catch to loadAsync for Font

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+// import { enableScreens } from 'react-native-screens';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+// enableScreens();
 
 const fetchFonts = () => {
   Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
+  }).catch(() => {});
 }
 
 export default function App() {
