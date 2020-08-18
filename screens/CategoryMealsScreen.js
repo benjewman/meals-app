90094ef7a77badde631ce31f1149db7697754f2c
benjewman <bennyadave@gmail.com>
2020-08-18 15:38:38 -0400

    Use meal dummy-data in CategoryMealsScreen to fill Flatlist

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        );
+    }
+    
+    const catId = props.navigation.getParam('categoryId');
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
     
     return (
         <View style={styles.screen}>
-            <Text>The Category Meal Screen</Text>
-            <Button title="Go to Details!" onPress={() => {
-                props.navigation.navigate('MealDetail');
-            }} />
-            <Button title="Go Back" onPress={() => {
-                props.navigation.goBack();
-            }} />
+            <FlatList data={displayedMeals} renderItem={renderMealItem}/>
         </View>
     );
 };
